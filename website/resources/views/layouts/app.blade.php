2cdadf0a9eacc044e5d8260ebfdeef9d3b9d6389
Wiebe <wieberan@hotmail.nl>
2020-01-28 11:03:50 +0100

    Change home&app view

--- website/resources/views/layouts/app.blade.php
+++ website/resources/views/layouts/app.blade.php
@@ -4,23 +4,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="">
     <title>BoomerShop</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('styles')
 </head>
 <body>
 
     <header>
-        {{-- Hier komt de header stuff --}}
+
     </header>
 
     <main>
-        {{-- Hier komt de website contents --}}
         @yield('content')
     </main>
 
     <footer>
-        {{-- Hier komt de footer stuff --}}
     </footer>
 
     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
